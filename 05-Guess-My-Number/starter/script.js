afaf60d52d2adf4e0d651a5383e11303668adbb7
manpreetkaur15 <manpreetkaur0699@gmail.com>
2022-02-12 23:25:34 +0530

    updated

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -3,4 +3,4 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 0;
 let highscore = 0;
 
-const displayMessage = function (message) {};
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
