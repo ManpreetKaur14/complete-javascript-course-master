2ba6410b633c0a9665c20a40f0d0aca40a364695
manpreetkaur15 <manpreetkaur0699@gmail.com>
2022-02-13 18:45:12 +0530

    update day 2

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,7 +1,28 @@
 'use strict';
+
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 100;
+// document.querySelector('.guess').value = 103;
+// console.log(document.querySelector('.guess').value);
+
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 0;
 let highscore = 0;
 
-// document.querySelector('.message').textContent = '🎉 Correct Number!';
-//
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    displayMessage('⛔️ No number!');
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.background = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  }
+});
