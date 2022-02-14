8d40a42021a34adca18818043cc0588cbfa61287
manpreetkaur15 <manpreetkaur0699@gmail.com>
2022-02-14 22:22:02 +0530

    DAY 3

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnclose = document.querySelector('.close-modal');
+const btnopen = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnopen[i].addEventListener('click', openModal);
+
+btnclose.addEventListener('click', closeModal);
+overlay.addEventListener('.click', closeModal);
